a52a883e13f4bde96f166ad72c8b8e33fe0a1c38
Matthew Richards <matthew.richards@zuhlke.com>
2017-03-23 11:44:41 +0000

    Jenkins initial configuration

--- vagrant/bootstrap-jenkins.sh
+++ vagrant/bootstrap-jenkins.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Wait for the jenkins instance to be up and running
+wait_for_jenkins(){
+    # Check the response for a success code
+    while [[ $(curl -s -w "%{http_code}"  http://localhost:8080 -o /dev/null) != "200" ]]; do
+       sleep 1
+    done
+}
+
+# Restart jenkins instance, pause script execution until it is back up and running
+restart_jenkins(){
+    sudo service jenkins restart
+    wait_for_jenkins
+}
+
 echo "--- Updating apt package lists ---"
 sudo apt-get -y update
 
@@ -13,3 +27,14 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get -y update
 sudo apt-get -y install jenkins
+
+# Copy jenkins configuration file (which is in the shared directory)
+echo "--- Configuring jenkins ---"
+sudo cp /jenkins-config/config.xml /var/lib/jenkins/config.xml
+
+# Disable the upgrade wizard by setting the version
+JENKINSVERSION=$(cat /var/lib/jenkins/config.xml | grep version\>.*\<\/version | grep -o [0-9\.]*)
+echo $JENKINSVERSION >> /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+
+# Restart jenkins
+restart_jenkins
