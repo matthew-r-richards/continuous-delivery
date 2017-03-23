185502dafb633dca41cdccebd2f17f4f8a22af69
Matthew Richards <matthew.richards@zuhlke.com>
2017-03-23 15:43:37 +0000

    Tidy up script

--- vagrant/bootstrap-jenkins.sh
+++ vagrant/bootstrap-jenkins.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+JENKINS_URL='http://127.0.0.1:8080/'
+JENKINS_PLUGINS_URL='https://updates.jenkins-ci.org/latest/'
+
 # Wait for the jenkins instance to be up and running
 wait_for_jenkins(){
     # Check the response for a success code
-    while [[ $(curl -s -w "%{http_code}"  http://127.0.0.1:8080 -o /dev/null) != "200" ]]; do
+    while [[ $(curl -s -w "%{http_code}"  $JENKINS_URL -o /dev/null) != "200" ]]; do
        sleep 1
     done
 }
@@ -33,18 +36,19 @@ echo "--- Configuring jenkins ---"
 sudo cp /jenkins-config/config.xml /var/lib/jenkins/config.xml
 
 # Disable the upgrade wizard by setting the version
-JENKINSVERSION=$(cat /var/lib/jenkins/config.xml | grep version\>.*\<\/version | grep -o [0-9\.]*)
-echo $JENKINSVERSION >> /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+JENKINS_VERSION=$(cat /var/lib/jenkins/config.xml | grep version\>.*\<\/version | grep -o [0-9\.]*)
+echo $JENKINS_VERSION >> /var/lib/jenkins/jenkins.install.UpgradeWizard.state
 
 # Restart jenkins
 restart_jenkins
 
 # Fetch the CLI client
-sudo wget -q http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
+sudo wget -q ${JENKINS_URL}jnlpJars/jenkins-cli.jar
 
 # Install jenkins plugins
 echo "--- Installing jenkins plugins ---"
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin git workflow-aggregator
+java -jar jenkins-cli.jar -s $JENKINS_URL install-plugin ${JENKINS_PLUGINS_URL}git.hpi ${JENKINS_PLUGINS_URL}workflow-aggregator.hpi
 
 # Restart jenkins safely (i.e. after plugin installation)
-java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ safe-restart
+java -jar jenkins-cli.jar -s $JENKINS_URL safe-restart
+java -jar jenkins-cli.jar -s $JENKINS_URL reload-configuration
