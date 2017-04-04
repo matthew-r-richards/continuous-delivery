a783eee64b7025516a77cc68c75f5c93323b1cfa
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-04 14:54:26 +0100

    First failing test

---
+++ apps/Server/WebApplication.tests/Repositories/TimesheetRepositoryFacts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.DatabaseContext;
+using WebApplication.Models;
+using WebApplication.Repositories;
+using Xunit;
+
+namespace Repositories
+{
+	public class TimesheetRepositoryFacts
+	{
+		public class Constructor
+		{
+			/// <summary>
+			/// Checks that the constructor throws an Exception if passed a null context.
+			/// </summary>
+			[Fact]
+			public void Throws_Null_Exception_For_Null_Context()
+			{
+				var ex = Assert.Throws<ArgumentNullException>(() => new TimesheetRepository(null));
+				Assert.Contains("context", ex.Message);
+			}
+		}
+
+		public class GetAll
+		{
+			[Fact]
+			public void Returns_All_Timesheet_Entries()
+			{
+			}
+
+			[Fact]
+			public void Returns_Empty_Collection_If_No_Timesheet_Entries_Exist()
+			{
+			}
+
+			private void AddTestData(IEnumerable<TimesheetEntry> data)
+			{
+				
+			}
+		}
+	}
+}
--- apps/Server/WebApplication/Repositories/TimesheetRepository.cs
+++ apps/Server/WebApplication/Repositories/TimesheetRepository.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using WebApplication.DatabaseContext;
 using WebApplication.Models;
 
 namespace WebApplication.Repositories
 {
 	public class TimesheetRepository : ITimesheetRepository
 	{
+		/// <summary>
+		/// The database context.
+		/// </summary>
+		private TimesheetContext context;
+
+		public TimesheetRepository(TimesheetContext context)
+		{
+			this.context = context;
+		}
+
 		/// <summary>
 		/// Gets all Timesheet Entries.
 		/// </summary>
