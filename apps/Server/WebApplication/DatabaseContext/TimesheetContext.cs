be955cb3d48a8b0f7277f19632efd7ab16d6e434
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-04 13:51:59 +0100

    Fix unit test stage, add xunit plugin

--- apps/Server/WebApplication/DatabaseContext/TimesheetContext.cs
+++ apps/Server/WebApplication/DatabaseContext/TimesheetContext.cs
@@ -6,7 +6,7 @@ namespace WebApplication.DatabaseContext
 	public class TimesheetContext : DbContext
 	{
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:WebApplication.TimesheetContext"/> class.
+		/// Initializes a new instance of the <see cref="T:WebApplication.DatabaseContext.TimesheetContext"/> class.
 		/// </summary>
 		/// <param name="options">The options to be used when creating the context.</param>
 		public TimesheetContext(DbContextOptions<TimesheetContext> options)
--- apps/Server/WebApplication/Repositories/ITimesheetRepository.cs
+++ apps/Server/WebApplication/Repositories/ITimesheetRepository.cs
@@ -5,6 +5,10 @@ namespace WebApplication.Repositories
 {
 	public interface ITimesheetRepository
 	{
+		/// <summary>
+		/// Gets all Timesheet Entries.
+		/// </summary>
+		/// <returns>A collection of <see cref="T:WebApplication.Model.TimesheetEntry"/>.</returns>
 		IEnumerable<TimesheetEntry> GetAll();
 	}
 }
