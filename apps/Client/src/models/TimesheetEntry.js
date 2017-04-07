0de7550a53434d182837cc2d0496a235f3e7419f
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-07 14:14:13 +0100

    TimesheetEntry class

---
+++ apps/Client/src/models/TimesheetEntry.js
@@ -0,0 +1,8 @@
+export default class TimesheetEntry {
+    constructor(name, description, start, end) {
+        this.name = name;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+}
