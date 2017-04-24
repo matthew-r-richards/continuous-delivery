707cc5a75807afc3605b1b35f22b90e67572a065
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-24 13:29:46 +0100

    Add code to pass test

--- apps/Client/src/components/EntryList.jsx
+++ apps/Client/src/components/EntryList.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+import Entry from 'components/Entry';
 
 export default class EntryList extends Component {
     render() {
-        return (
-            <Panel>
-                some list
-            </Panel>
-        )
+        const entriesExist = this.props.entries && this.props.entries.length > 0;
+        let content;
+        if (entriesExist) {
+            content = this.props.entries.map((entry, index) => (
+                <Entry key={index}/>));
+        } else {
+            content = 'No timesheet entries defined...';
+        }
+        return this.props.entries ?
+            (<Panel>
+                {content}
+            </Panel>)
+        : null;
     }
 }
+
+EntryList.propTypes = {
+    entries: React.PropTypes.array.isRequired
+} 
