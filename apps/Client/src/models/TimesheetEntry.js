524d1d36d28586536c663d6df2d17a4cdebf0d09
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-25 15:08:56 +0100

    Remove dependency on TimesheetEntry model. Make POST call to WebAPI to add an entry

--- apps/Client/src/models/TimesheetEntry.js
+++
@@ -1,8 +0,0 @@
-export default class TimesheetEntry {
-    constructor(name, description, start, end) {
-        this.name = name;
-        this.description = description;
-        this.start = start;
-        this.end = end;
-    }
-}
--- apps/Client/src/utils/ApiUtils.js
+++ apps/Client/src/utils/ApiUtils.js
@@ -1,6 +1,5 @@
 import serverActionCreators from 'actions/ServerActionCreators';
 import { APIEndpoints } from 'constants/ApiConstants';
-import TimesheetEntry from 'models/TimesheetEntry';
 
 import Request from 'superagent';
 
@@ -12,7 +11,7 @@ export default {
         Request
             .get(endpoint)
             .set('Accept', 'application/json')
-            .end(function(err, response) {
+            .end((err, response) => {
                 if (err) return console.error(err);
                 serverActionCreators.receiveEntries(response.body);
             });
@@ -20,7 +19,15 @@ export default {
 
     addEntry: (name, description) => {
         const endpoint = APIEndpoints.ENTRIES;
-        console.log(`API Endpoint: POST ${endpoint}, Name: ${name}, Description: ${description}`);
-        serverActionCreators.receiveAddedEntry(new TimesheetEntry(name, description, new Date(), null));
+        console.log(`API Endpoint: POST ${endpoint}, taskName: ${name}, taskDescription: ${description}`);
+
+        Request
+            .post(endpoint)
+            .set('Accept', 'application/json')
+            .send({ taskName: name, taskDescription: description })
+            .end((err, response) => {
+                if (err) return console.error(err);
+                serverActionCreators.receiveAddedEntry(response.body);
+            });
     }
 }
