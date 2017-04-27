153541adfb265c9616bc2dffc58a3afc260dbf67
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-27 11:22:08 +0100

    Add new logging capability to avoid console output when unit testing

---
+++ apps/Client/src/utils/Logger.js
@@ -0,0 +1,40 @@
+import winston from 'winston';
+
+let logger;
+
+if (process.env.NODE_ENV === 'test') {
+    // only log to file when testing, so that we don't fill up the console
+    logger = new (winston.Logger)({
+        transports: [
+            new (winston.transports.File)({
+                filename: 'logs/client-app-testing.log',
+                maxsize: 1048576,
+                maxfiles: 5,
+                tailable: true,
+                json: false
+            })
+        ]
+    });
+} else if (process.env.NODE_ENV === 'production') {
+    logger = new (winston.Logger)({
+        transports: [
+            new (winston.transports.Console)(),
+            new (winston.transports.File)({
+                filename: 'logs/client-app.log',
+                maxsize: 1048576,
+                maxfiles: 5,
+                tailable: true,
+                json: false
+            })
+        ]
+    });
+} else {
+    // only log to the console in development
+    logger = new (winston.Logger)({
+        transports: [
+            new (winston.transports.Console)()
+        ]
+    });
+}
+
+export default logger;
