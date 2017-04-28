29b8626bf73d764a30d5e42e5658bac8c38c3279
Matthew Richards <matthew.richards@zuhlke.com>
2017-04-28 12:34:58 +0100

    Update elapsed time on a regular basis

--- apps/Client/src/components/Entry.jsx
+++ apps/Client/src/components/Entry.jsx
@@ -8,6 +8,15 @@ export default class Entry extends Component {
 
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
         this.handleStopClick = this.handleStopClick.bind(this);
+        this.calculateElapsedTime = this.calculateElapsedTime.bind(this);
+
+        this.state = {
+            elapsedTime: ''
+        }
+    }
+    
+    componentDidMount() {
+        this.calculateElapsedTime();
     }
 
     handleStopClick() {
@@ -17,23 +26,54 @@ export default class Entry extends Component {
     handleDeleteClick() {
         this.props.onDelete(this.props.data.id);
     }
+
+    calculateElapsedTime() {
+        console.log('in calculated elapsed time');
+        const entry = this.props.data;
+        const endTime = entry.taskEnd ? moment(entry.taskEnd) : moment();
+        const diffInMinutes = endTime.diff(moment(entry.taskStart), 'minutes');
+        const hours = Math.floor(diffInMinutes / (60));
+        const minutes = hours > 0 ? diffInMinutes % (60 * hours) : diffInMinutes;
+        let elapsedTime;
+
+        if (hours === 0 && minutes === 0) {
+            elapsedTime = '0m';
+        } else {
+            elapsedTime = (hours > 0 ? `${hours}h ` : '') + (minutes > 0 ? `${minutes}m` : '');
+        }
+
+        if (entry.taskEnd) {
+            this.setState({
+                elapsedTime: elapsedTime
+            });
+        } else {
+            // if we don't have an end time defined, we need to update the elapsedTime regularly
+            this.setState({
+                elapsedTime: elapsedTime
+            }, () => {
+                setTimeout(this.calculateElapsedTime, 5000)
+            })
+        }
+    }
     
     render() {
         const entry = this.props.data;
-        const formattedStartTime = moment(entry.taskStart).format('h:m A');
+        const formattedStartTime = moment(entry.taskStart).format('h:mm A');
         const formattedEndTime = entry.taskEnd ? moment(entry.taskEnd).format('h:mm A') : 'In progress...';
-        const displayStop = !entry.taskEnd;
+        const completeEntry = entry.taskEnd;
+        const rowClass = completeEntry ? 'complete' : 'incomplete';
 
         return (
             <Grid>
-                <Row>
-                    <Col md={2} className="entry-name">{entry.taskName}</Col>
-                    <Col md={2} className="entry-details">{entry.taskDescription}</Col>
-                    <Col md={2} className="entry-details">{formattedStartTime}</Col>
-                    <Col md={2} className="entry-details">{formattedEndTime}</Col>
-                    <Col md={2} mdPush={2}>
+                <Row className={rowClass}>
+                    <Col md={3} className="entry-name">{entry.taskName}</Col>
+                    <Col md={3} className="entry-details">{entry.taskDescription}</Col>
+                    <Col md={1} className="entry-details">{formattedStartTime}</Col>
+                    <Col md={1} className="entry-details">{formattedEndTime}</Col>
+                    <Col md={1} className="entry-details">{this.state.elapsedTime}</Col>
+                    <Col md={2} mdPush={1}>
                         <ButtonToolbar>
-                            { displayStop && <Button id="stopBtn" bsSize="small" onClick={this.handleStopClick}><Glyphicon glyph="stop" /></Button> }
+                            { !completeEntry && <Button id="stopBtn" bsSize="small" onClick={this.handleStopClick}><Glyphicon glyph="stop" /></Button> }
                             <Button id="deleteBtn" bsStyle="danger" bsSize="small" onClick={this.handleDeleteClick}><Glyphicon glyph="remove" /></Button>
                         </ButtonToolbar>
                     </Col>
--- apps/Client/test/components/Entry.spec.js
+++ apps/Client/test/components/Entry.spec.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import { Col, Button, Glyphicon } from 'react-bootstrap';
-import { spy } from 'sinon';
+import { spy, useFakeTimers } from 'sinon';
 
 import Entry from 'components/Entry';
 
 describe('<Entry/>', () => {
   let wrapper;
-  let onDeleteSpy, onStopSpy;
-  const amTime = new Date();
-  amTime.setHours(9);
-  amTime.setMinutes(30);
-  const pmTime = new Date();
-  pmTime.setHours(16);
-  pmTime.setMinutes(59);
+  let onDeleteSpy, onStopSpy, calculateSpy;
+  let amTime, pmTime;
+  let fakeClock;
 
   beforeEach(() => {
     onDeleteSpy = spy();
     onStopSpy = spy();
+    calculateSpy = spy(Entry.prototype, 'calculateElapsedTime');
+
+    // use the sinon fake timers functionality so that we can control the execution timeline
+    fakeClock = useFakeTimers();
+
+    amTime = new Date();
+    amTime.setHours(9);
+    amTime.setMinutes(30);
+    pmTime = new Date();
+    pmTime.setHours(16);
+    pmTime.setMinutes(59);
+  });
+
+  afterEach(() => {
+    calculateSpy.restore();
+    
+    // restore the clock so we don't affect other tests
+    fakeClock.restore();
   });
 
   it('should display entry details', () => {
@@ -42,6 +56,85 @@ describe('<Entry/>', () => {
       [
         <Col>9:30 AM</Col>,
         <Col>In progress...</Col>
+      ])).to.be.true;
+  });
+
+  it('should display elapsed time on start if no end time is specified', () => {
+    // set up for an elapsed time of 15 minutes
+    const startTime = new Date();
+    startTime.setMinutes(startTime.getMinutes() - 15);
+    const entry = { id: 1, taskName: 'new name', taskDescription: 'new description', taskStart: startTime, taskEnd: null };
+    wrapper = mount(<Entry data={entry} onDelete={onDeleteSpy} onStop={onStopSpy}/>);
+
+    expect(calculateSpy.calledOnce).to.be.true;
+
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>15m</Col>
+      ])).to.be.true;
+  });
+
+  it('should display duration on start if an end time is specified', () => {
+    // set up for a duration time of 90 minutes (1h 30m)
+    const startTime = new Date();
+    const endTime = new Date();
+    startTime.setMinutes(startTime.getMinutes() - 90);
+    const entry = { id: 1, taskName: 'new name', taskDescription: 'new description', taskStart: startTime, taskEnd: endTime };
+    wrapper = mount(<Entry data={entry} onDelete={onDeleteSpy} onStop={onStopSpy}/>);
+
+    expect(calculateSpy.calledOnce).to.be.true;
+
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>1h 30m</Col>
+      ])).to.be.true;
+  });
+
+  it('should update elapsed time every 5s if no end time is specified', () => {
+    const startTime = new Date();
+    startTime.setMinutes(startTime.getMinutes() - 15);
+    const entry = { id: 1, taskName: 'new name', taskDescription: 'new description', taskStart: startTime, taskEnd: null };
+    wrapper = mount(<Entry data={entry} onDelete={onDeleteSpy} onStop={onStopSpy}/>);
+    calculateSpy.reset();
+
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>15m</Col>
+      ])).to.be.true;
+
+    fakeClock.tick(5000);
+    expect(calculateSpy.calledOnce).to.be.true;
+
+    fakeClock.tick(5000);
+    expect(calculateSpy.calledTwice).to.be.true;
+
+    // check the time has been updated after a minute (we've already had 10s)
+    fakeClock.tick(50000);
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>16m</Col>
+      ])).to.be.true;
+  });
+
+  it('should not update duration (i.e. end time is specified) after the initial calculation', () => {
+    const startTime = new Date();
+    startTime.setMinutes(startTime.getMinutes() - 15);
+    const endTime = new Date();
+    const entry = { id: 1, taskName: 'new name', taskDescription: 'new description', taskStart: startTime, taskEnd: endTime };
+    wrapper = mount(<Entry data={entry} onDelete={onDeleteSpy} onStop={onStopSpy}/>);
+    calculateSpy.reset();
+
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>15m</Col>
+      ])).to.be.true;
+
+    fakeClock.tick(600000) // 1 hour
+    expect(calculateSpy.notCalled).to.be.true;
+
+    expect(wrapper.containsAllMatchingElements(
+      [
+        <Col>15m</Col>
       ])).to.be.true;
   });
 
