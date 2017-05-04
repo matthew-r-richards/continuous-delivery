439e7b4d108798cfd85e0629b1040e322aa4c608
Matthew Richards <matthew.richards@zuhlke.com>
2017-05-04 12:43:49 +0100

    Install puppet modules for both environments

--- vagrant/bootstrap-puppet-master.sh
+++ vagrant/bootstrap-puppet-master.sh
@@ -39,8 +39,10 @@ echo "--- Installing puppet server ---"
 sudo apt-get -y install puppetserver
 
 echo "--- Installing puppet modules ---"
-/opt/puppetlabs/bin/puppet module install puppetlabs-ntp
-/opt/puppetlabs/bin/puppet module install garethr-docker
+sudo /opt/puppetlabs/bin/puppet module install --environment production puppetlabs-ntp
+sudo /opt/puppetlabs/bin/puppet module install --environment production garethr-docker
+sudo /opt/puppetlabs/bin/puppet module install --environment qa puppetlabs-ntp
+sudo /opt/puppetlabs/bin/puppet module install --environment qa garethr-docker
 
 echo "--- Updating the server memory allocation ---"
 sudo sed -i 's/-Xms.*\s-Xmx.*\s/-Xms512m -Xmx512m /' /etc/default/puppetserver
